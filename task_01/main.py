bafd3093f63dde18c33dbbeca070bebcc1f3192f
jixiang <li_jixiang723@163.com>
2021-08-04 11:42:38 +0800

    init

---
+++ task_01/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/task_01")
+def task_01():
+    return "<h1>Task_01</h1>"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="10103")
