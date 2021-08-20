fed947c8e868e690e155ad29801f20b0282e0861
lijixiang_home <1597123514@qq.com>
2021-08-20 21:53:16 +0800

    modify main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/task_01")
 def task_01():
-    return "<h1>Task_01 and Jenkins Success!!! hello world</h1>"
+    return "<h1>Task_01 and Jenkins Success!!! hello world 老爸平安无事</h1>"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="10103")
+    app.run(host="0.0.0.0", port="10104")
